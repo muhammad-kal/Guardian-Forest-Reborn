a20d4a4bb604e90917d6b5d95768cd6b385177fd
Muhammad Haikal <muhammad.haikal494972@gmail.com>
2025-07-27 23:07:06 +0700

    HALO

--- Assets/GuardianForestReborn/Scripts/Tanaman/Tanaman.cs
+++ Assets/GuardianForestReborn/Scripts/Tanaman/Tanaman.cs
@@ -20,17 +20,31 @@ public class Tanaman : MonoBehaviour
     }
     public void Terbakar()
     {
-        // Ubah warna menjadi oranye-merah seperti terbakar
+        //// Ubah warna menjadi oranye-merah seperti terbakar
+        //Renderer renderer = jagungRendererTransform.GetComponentInChildren<Renderer>();
+        //if (renderer != null)
+        //{
+        //    renderer.gameObject.LeanColor(Color.white * 2f, 1);
+        //    Debug.Log("b");
+        //}
+        //// (Opsional) Tambah animasi gemetar seperti terbakar
+        //// jagungRendererTransform.gameObject.LeanPunchScale(Vector3.one * 0.2f, 1f, 10, 1f);
+
+        //// (Opsional) Tambah partikel api jika kamu punya prefab-nya
+        //Instantiate(apiPrefab, jagungRendererTransform.position, Quaternion.identity, jagungRendererTransform);
+
+
         Renderer renderer = jagungRendererTransform.GetComponentInChildren<Renderer>();
         if (renderer != null)
         {
-            renderer.gameObject.LeanColor(Color.white * 2f, 1);
+            // Paksa Unity untuk membuat salinan material agar tidak shared
+            Material instanceMat = renderer.material;
+            renderer.material = instanceMat;
+
+            // Baru ubah warnanya dengan LeanTween
+            renderer.gameObject.LeanColor(Color.black , 1);
+
             Debug.Log("b");
         }
-        // (Opsional) Tambah animasi gemetar seperti terbakar
-        // jagungRendererTransform.gameObject.LeanPunchScale(Vector3.one * 0.2f, 1f, 10, 1f);
-
-        // (Opsional) Tambah partikel api jika kamu punya prefab-nya
-        // Instantiate(apiPrefab, jagungRendererTransform.position, Quaternion.identity, jagungRendererTransform);
     }
 }
