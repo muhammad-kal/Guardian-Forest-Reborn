65767decd90b11372c3787ebe7ffe897660d0746
Rayyan <60314224+RayyanHermanto@users.noreply.github.com>
2025-08-07 15:53:28 +0700

    Update Inventory.cs

--- Assets/GuardianForestReborn/Scripts/Player/Inventory.cs
+++ Assets/GuardianForestReborn/Scripts/Player/Inventory.cs
@@ -37,7 +37,6 @@ public class Inventory : MonoBehaviour
                 {
                     AmbilItemLangsung(other);
                 }
-
                 lastItemTag = other.tag;
             }
         }
